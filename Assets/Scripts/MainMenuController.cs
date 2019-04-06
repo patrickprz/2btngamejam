7c806239b23a2f4e681bea5f1e3a12e87fcb8242
Adriel <aadriieel@gmail.com>
2019-04-05 22:39:45 -0300

    main menu UI

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    #region Events
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("play");
+            Play();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("exit");
+            Exit();
+        }
+    }
+    #endregion
+
+    #region Methods
+    public void Play()
+    {
+        SceneManager.LoadScene("game");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
