8cebb64b78df17db65c985ecfb7cc51ffbe02f17
Adriel <aadriieel@gmail.com>
2019-04-06 10:22:44 -0300

    comecei a fazer a parte do spawn dos items

---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour
+{
+    [SerializeField]
+    private float TimeToDie;
+
+    void Start()
+    {
+        Destroy(gameObject, TimeToDie);
+    }
+
+}
---
+++ Assets/Scripts/ItemSpawnerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawnerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] Items;
+    [SerializeField]
+    private float YPositionMultiplier;
+    private float CurrentlyYPosition;
+    [SerializeField]
+    private float SpawnCooldown;
+    private float SpawnCooldownTimeLeft;
+    void Start()
+    {
+        CurrentlyYPosition = 6f;
+        SpawnCooldownTimeLeft = 0f;
+    }
+
+
+    void Update()
+    {
+        SpawnCooldownTimeLeft -= Time.deltaTime;
+        if (SpawnCooldownTimeLeft <= 0)
+        {
+            SpawnItem();
+            SpawnCooldownTimeLeft = SpawnCooldown;
+        }
+    }
+
+    public void SpawnItem()
+    {
+        int randonRange = Random.Range(0, Items.Length);//vai o length mesmo porque o range com int é inclusivo para o inicial, mas exclusivo para o final
+
+        Instantiate(Items[randonRange], new Vector3(0f, CurrentlyYPosition, 0f), this.transform.rotation);
+        CurrentlyYPosition += YPositionMultiplier;
+
+    }
+}
